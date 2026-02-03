a14e8579c93f46aef41c530af98ee2fb26379056
uday-chavan <chavanuday407@gmail.com>
2026-02-03 11:03:57 +0530

    Update

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "@/hooks/use-auth";
+ import { useAuth } from "@/hooks/use-auth";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
